30b4dca37db4490feb94eb2cf3073dc989a1cd04
hiro8787 <hirokun0530@gmail.com>
2024-04-21 00:51:59 +0900

    commit

--- src/resources/views/login.blade.php
+++ src/resources/views/login.blade.php
@@ -2,14 +2,43 @@
 <html lang="ja">
 
 <head>
-  <meta charset="UTF-8">
-  <meta http-equiv="X-UA-Compatible" content="IE=edge">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>Document</title>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="{{ asset('css/login.css') }}">
+    <title>Atte</title>
 </head>
 
 <body>
-  <h1>Atte</h1>
+    <header>
+        <div class="header">
+            <div class="header-logo">Atte</div>
+        </div>
+    </header>
+    <main>
+        <div class="main">
+            <div class="main-item">ログイン
+            <form class="form" name="login" method="POST" action="/stamp">
+                @csrf
+                <div class="form-item">
+                    <div class="form-item-text">
+                        <input type="email" name="email" class="form-item-input" placeholder="メールアドレス" value="{{ old('email') }}"/>
+                    </div>
+                    <div class="form-item-text">
+                        <input type="password" name="password" class="form-item-input" placeholder="パスワード" value="{{ old('password') }}"/>
+                    </div>
+                    <div class="form-item-category">
+                        <button type="submit" class="form-item-btn">ログイン</button>
+                    </div>
+                    </div>
+                    <div class="form-guidance">アカウントをお持ちでない方はこちらから</div>
+                    <a href="http://localhost/register/" class="entry-item">会員登録</a>
+                
+            </form>
+    </main>
+    <footer>
+        <div class="footer-logo">Atte,inc.</div>
+    </footer>
 </body>
 
 </html>
